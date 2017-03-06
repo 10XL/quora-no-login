43c98f12b96b4e116ab649f637aba5610e5bb07f
10xl <urnotuurmi@gmail.com>
2017-03-06 13:42:43 -0500

    check if 'top stories' card clicked

--- quora-no-login/content.js
+++ quora-no-login/content.js
@@ -5,7 +5,8 @@ if (document.getElementsByClassName('signup_wall_content')[0]) {
 }
 
 window.addEventListener('click', function(event) {
-  let questionLink = findLink(event.path);
+  let questionLink = cardLink(event.target) || findLink(event.path);
+  console.log('event is: ', event);
   if (questionLink) {
     chrome.runtime.sendMessage({
       reload: true,
@@ -18,4 +19,11 @@ function findLink(arr) {
   for (let i = 0; i < arr.length; i++)
     if (arr[i].className === 'question_link')
       return 'https://www.quora.com' + arr[i].attributes.href.value;
+}
+
+function cardLink(card) {
+  if (card.className === 'feed_item_overlay')
+    return card.attributes.href.value;
+  if (card.className === 'read_more_link')
+    return card.parentNode.attributes.href.value;
 }
